651ae802a3b61680e59264b0050b156e76746263
MASDXI <sirawitt42@gmail.com>
2024-01-22 16:21:21 +0700

    refactor

--- task/helpers/helper.ts
+++ task/helpers/helper.ts
@@ -4,13 +4,18 @@ import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 export const loadCommitteContract = async (hre: HardhatRuntimeEnvironment) => {
-    return await hre.ethers.getContractAt("Committee", process.env.COMMITEE_CONTRACT_ADDRESS ?
-    process.env.COMMITEE_CONTRACT_ADDRESS:"0x0000000000000000000000000000000000000069") 
+    return await hre.ethers.getContractAt("Committee", 
+        "0x0000000000000000000000000000000000000776");
 }
 
 export const loadSupplyControlContract = async (hre: HardhatRuntimeEnvironment) => {
-    return await hre.ethers.getContractAt("SupplyControl", process.env.SUPPLY_CONTROL_CONTRACT_ADDRESS ? 
-    process.env.SUPPLY_CONTROL_CONTRACT_ADDRESS:"0x0000000000000000000000000000000000000070")
+    return await hre.ethers.getContractAt("SupplyControl", 
+        "0x0000000000000000000000000000000000000777");
+}
+
+export const loadTxFeeDistributorContract = async (hre: HardhatRuntimeEnvironment) => {
+    return await hre.ethers.getContractAt("SupplyControl", 
+        "0x0000000000000000000000000000000000000778");
 }
 
 export const getProposerSigner = async (account: SignerWithAddress) => {
--- test/utils/systemContractFixture.ts
+++ test/utils/systemContractFixture.ts
@@ -23,15 +23,17 @@ export async function setSystemContractFixture() {
     
   // pre-mint native token for initializer.
   await setBalance(constants.INITIALIZER_ADDRESS, constants.ONE_HUNDRED_TOKEN);
-  await setBalance(constants.SUPPLY_CONTRACT_ADDRESS,constants.ONE_TRILLION_TOKEN);
+  await setBalance(constants.TREASURY_CONTRACT_ADDRESS, constants.ONE_TRILLION_TOKEN);
 
   // set contract code to preload contract address.
-  await setCode(constants.SUPPLY_CONTRACT_ADDRESS, constants.SUPPLY_CONTRACT_BIN);
+  await setCode(constants.TREASURY_CONTRACT_ADDRESS, constants.SUPPLY_CONTRACT_BIN);
   await setCode(constants.COMMITTEE_CONTRACT_ADDRESS, constants.COMMITEE_CONTRACT_BIN);
 
   // load contract from to address
-  const supplycontrol = await ethers.getContractAt("TreasuryContract",constants.SUPPLY_CONTRACT_ADDRESS);
-  const committee = await ethers.getContractAt("Committee",constants.COMMITTEE_CONTRACT_ADDRESS);
+  const supplycontrol = await ethers.getContractAt("TreasuryContract",
+    constants.TREASURY_CONTRACT_ADDRESS);
+  const committee = await ethers.getContractAt("Committee",
+    constants.COMMITTEE_CONTRACT_ADDRESS);
   await mine(200);
   return { committee, admin , committee1, committee2, committee3, proposer1 , proposer2 , otherAccount, initializerCallerSigner, supplycontrol };
 }
