234dfbecbf8d14c369b12fad2a62209b23d3d9e8
Anchisa Pinyo <anchisan@NBAAP2100004.local>
2024-02-02 12:13:30 +0700

    add get_committee_count.ts

---
+++ task/committee/get_committee_count.ts
@@ -0,0 +1,13 @@
+import { task } from "hardhat/config"
+import { loadCommitteContract } from "../helpers/helper"
+
+task("get_committee_count", "check committee number")
+  .setAction(async (args, hre) => {
+    const committee = await loadCommitteContract(hre)
+    try {
+        const tx = await committee.getCommitteeCount();
+        console.log(tx)
+    } catch (err) {
+        console.error(err)
+    }
+})
