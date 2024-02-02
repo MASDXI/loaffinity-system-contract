fb5bbfa7e30f6748fd76cf96ec70866c039094e3
Anchisa Pinyo <anchisan@NBAAP2100004.local>
2024-02-02 12:15:15 +0700

    revise vote.ts

--- task/helpers/vote.ts
+++ task/helpers/vote.ts
@@ -1,13 +1,13 @@
 import { task } from "hardhat/config"
-import { loadCommitteContract, loadSupplyControlContract } from "../helpers/helper";import { ContractTransactionResponse, TransactionResponse } from "ethers";
- "../helpers/helper"
+import { loadCommitteContract, loadSupplyControlContract } from "../helpers/helper";
+import { ContractTransactionResponse, TransactionResponse } from "ethers";
 
 task("vote", "vote proposal")
-  .addParam("proposal", "destination address")
+  .addParam("proposal", "destination ")
   .addParam("auth", "auth")
   .addParam("contract","0:committe 1:supply")
   .setAction(async (args, hre) => {
-    const proposal = String(args.account)
+    const proposal = String(args.proposal)
     const auth = Boolean(args.auth)
     const contract = Number(args.contract)
     let res: any
@@ -31,4 +31,3 @@ task("vote", "vote proposal")
           break;
       }
   })
-
