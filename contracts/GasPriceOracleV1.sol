d5c61fcc4a9c52e72b3cdb1a94d23e1bf4a94c0d
MASDXI <sirawitt42@gmail.com>
2024-02-21 11:13:46 +0700

    more clean up

--- contracts/GasPriceOracleV1.sol
+++ contracts/GasPriceOracleV1.sol
@@ -7,20 +7,25 @@ pragma solidity 0.8.17;
 // adding to keep tracking active validator length
 
 import "./abstracts/Initializer.sol";
-import "./interfaces/IGasPriceOracleV1.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract GasPriceOracleV1 is IGasPriceOracleV1, Initializer {
+contract GasPriceOracleV1 is IGasPriceOracleV1, Initializer, Pausable {
+    enum PARAMETERS {
+        CEC,
+        CO2P,
+        SCR
+    }
 
-    enum PARAMETERS { CEC, CO2P, SCR }
-    enum FLAG { DISABLE, ENABLE }
+    event ParameterUpdated(
+        uint256 indexed blockNumber,
+        string indexed params,
+        uint256 value
+    );
 
-    event ParameterUpdated(uint256 indexed blockNumber, string indexed params, uint256 value);
     event ParameterBlockPeriodUpdate(uint32 blockperiod);
-    event Enabled();
-    event Disabled();
 
-    uint32 constant private ONE_YEAR = 31_536_000; // 1 year in seconds.
-    uint32 constant private ONE_HOUR = 3_600;      // 1 hour in seconds.
+    uint32 private constant ONE_YEAR = 31_536_000; // 1 year in seconds.
+    uint32 private constant ONE_HOUR = 3_600; // 1 hour in seconds.
 
     struct ConfigurationParemeter {
         /* Carbon Emission per Kilowatt hour Coefficient*/
@@ -38,97 +43,117 @@ contract GasPriceOracleV1 is IGasPriceOracleV1, Initializer {
     }
 
     /* Contract configuration */
-    FLAG private _flag;
     ConfigurationParemeter private _config;
     uint256 private _constant;
     uint256 private _lastUpdatedBlock;
-    
+
     /* Blockchain environment configuration */
     uint256 private _blockPeriod; // block period in seconds.
 
-
-    function initialize() public onlyInitializer {
+    // Example config
+    // _config.carbonEmissionCoefficient = 1; // no decimal
+    // _config.carbonCaptureCost = 344000000; // decimal 9
+    // _config.chargeRate = 1; // no decimal
+    // _config.c = 15748000000; // decimal 9
+    // _config.numberOfValidator = 4; // no decimal
+    // _config.k = 300; // decimal 9
+    // setBlockPeriod(15); // decimal 9 _15/(ONE_HOUR * 1000)
+    // _constant = 278;
+    function initialize(
+        uint256 _carbonEmissionCoefficient,
+        uint256 _carbonCaptureCost,
+        uint256 _chargeRate,
+        uint256 _c,
+        uint256 _numberOfValidator,
+        uint256 _k,
+        uint32 _blockPeriod
+    ) public onlyInitializer {
         _initialized();
-        _config.carbonEmissionCoefficient = 1;  // no decimal
-        _config.carbonCaptureCost = 344000000;  // decimal 9
-        _config.chargeRate = 1;                 // no decimal
-        _config.c = 15748000000;                // decimal 9
-        _config.numberOfValidator = 4;          // no decimal
-        _config.k = 300;                        // decimal 9
-        setBlockPeriod(15);                     // decimal 9 _15/(ONE_HOUR * 1000)
-        _constant = 278;
-
+        ConfigurationParemeter memory cacheConfig = ConfigurationParemeter(
+            _carbonEmissionCoefficient,
+            _carbonCaptureCost,
+            _chargeRate,
+            _c,
+            _numberOfValidator,
+            _k
+        );
+        _setConfiguration(cacheConfig);
+        _setBlockPeriod(_blockPeriod);
+        _pause();
         _lastUpdatedBlock = block.number;
     }
 
-    function enable() public override {
-        require(isInit(),
-            "GasPriceOracleV1: need to initialize before enable");
-        if (_flag == FLAG.ENABLE) {
-            _flag = FLAG.DISABLE;
-            emit Disabled();
-        } else {
-            _flag = FLAG.ENABLE;
-            emit Enabled();
-        }
-    }
-
-    function _parameterSelector(PARAMETERS param) private pure returns (string memory) {
-        require(uint8(param) < 3);
-        if (param == PARAMETERS.CEC) return "CEC";
-        if (param == PARAMETERS.CO2P) return "CO2P";
-        if (param == PARAMETERS.SCR) return "SCR";
-    }
-
-    function setValue(uint32 cec, uint32 co2p, uint32 scr) public override {
-        // Annaul Update Parameter
-        uint256 blockNumberCache = block.number;
-        require(blockNumberCache - getLastUpdatedBlock() >= ONE_YEAR,
-            "GasPriceOracleV1: It's not yet time to update values.");
-
-        _config.carbonEmissionCoefficient = cec;
-        _config.carbonCaptureCost = co2p;
-        _config.chargeRate = scr;
-        
-        emit ParameterUpdated(blockNumberCache, 
-            _parameterSelector(PARAMETERS.CEC), value1);
-        emit ParameterUpdated(blockNumberCache, 
-            _parameterSelector(PARAMETERS.CO2P), value2);
-        emit ParameterUpdated(blockNumberCache, 
-            _parameterSelector(PARAMETERS.SCR), value3);
-        
-        _lastUpdatedBlock = blockNumberCache;
-    }
-
-    function setBlockPeriod(uint32 blockPeriod) public override {
-        require(blockPeriod > 0, 
-            "GasPriceOracleV1: block period can't be zero");
-        require(_blockPeriod != blockPeriod, 
-            "GasPriceOracleV1: block period value exist");
-        _blockPeriod = blockPeriod;
-
-        emit ParameterBlockPeriodUpdate(blockPeriod);
-    }
-
-    function getBlockPeriod() public override view returns (uint256) {
+    function getBlockPeriod() public view override returns (uint256) {
         return _blockPeriod;
     }
 
-    function getLastUpdatedBlock() public override view returns(uint256) {
+    function getLastUpdatedBlock() public view override returns (uint256) {
         return _lastUpdatedBlock;
     }
 
-    function calculateTransactionFee(uint256 gasLimit) public override view returns (uint256) {
+    function calculateTransactionFee(
+        uint256 gasLimit
+    ) public view override returns (uint256) {
         // Carbon Emission Kg/kWh
         // uint256 carbonEmission = (_CEC * _blockPeriod /*_CO2P * *//(ONE_HOUR * 1000));
         // Calculate carbon emission
-        uint256 carbonEmission = (_config.carbonEmissionCoefficient * _blockPeriod) * _constant;
+        uint256 carbonEmission = (_config.carbonEmissionCoefficient *
+            _blockPeriod) * _constant;
         // Calculate validator contribution
         uint256 validatorContribution = (_config.c * _config.numberOfValidator);
         // Calculate charge rate
         uint256 chargeRate = (1 + _config.chargeRate);
         // Calculate total transaction fee
-        uint256 transactionFee = (_config.k * gasLimit + validatorContribution) * chargeRate * carbonEmission;
+        uint256 transactionFee = (_config.k *
+            gasLimit +
+            validatorContribution) *
+            chargeRate *
+            carbonEmission;
         return transactionFee;
+    }
+
+    // @TODO permission
+    function setConfiguration(
+        ConfigurationParemeter memory config
+    ) public {
+        // Annaul Update Parameter
+        uint256 blockNumberCache = block.number;
+        require(
+            blockNumberCache - getLastUpdatedBlock() >= ONE_YEAR,
+            "GasPriceOracleV1: It's not yet time to update values."
+        );
+        // @TODO require check new config
+        // should revert before cacheOldConfig for gas saving
+
+        ConfigurationParemeter memory cacheOldConfig = _config;
+        _config = config;
+        _lastUpdatedBlock = blockNumberCache;
+
+        emit configurationUpdated(cacheOldConfig, config);
+    }
+
+    // @TODO permission
+    function setBlockPeriod(uint32 blockPeriod) public {
+        require(
+            blockPeriod > 0,
+            "GasPriceOracleV1: block period can't be zero"
+        );
+        require(
+            _blockPeriod != blockPeriod,
+            "GasPriceOracleV1: block period value exist"
+        );
+        _blockPeriod = blockPeriod;
+
+        emit ParameterBlockPeriodUpdate(blockPeriod);
+    }
+    
+    // @TODO permission
+    function enable() public {
+        _unpause();
+    }
+    
+    // @TODO permission
+    function disable() public {
+        _pause();
     }
 }
